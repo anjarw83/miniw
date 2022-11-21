c036ae91f0f2b311d2d273af0668a0b12491cd8e
anjarw83 <anjar.office@gmail.com>
2022-11-21 08:43:20 +0700

    adding wallet unit test

---
+++ test/wallet/wallet.service.spec.js
@@ -0,0 +1,120 @@
+const walletService = require("../../src/wallet/wallet.service");
+const {MongoMemoryServer} = require("mongodb-memory-server");
+const {MongoClient} = require("mongodb");
+const Wallet = require("../../src/wallet/wallet.model");
+const Mutation = require("../../src/mutations/mutation.model");
+
+describe('wallet.service', () => {
+    let mongoServer, mongoClient , db;
+    const collectionName = 'wallet';
+
+    const crateInitialData = async (data) => {
+        db = mongoClient.db(mongoServer.instanceInfo.dbName);
+
+        const col = db.collection(collectionName);
+        await col.insertMany(data);
+    }
+
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        mongoClient = await MongoClient.connect(mongoServer.getUri(), {});
+    });
+
+    afterAll(async() => {
+        if (mongoClient) {
+            await mongoClient.close();
+        }
+        if (mongoServer) {
+            await mongoServer.stop();
+        }
+    });
+
+    describe('Deposits', () =>{
+
+        it('should success', async () => {
+
+            // Given
+            const initData = {
+                customerId: 'abc123',
+                balance: 50000,
+                enabled: true,
+                createdAt: Date.now(),
+            };
+            const params = {
+                xid: 'abc123',
+                amount: 50000,
+                reference_id: 'referenceid123'
+            }
+            const sumData = {
+                customerId: 'abc123',
+                balance: 100000,
+                enabled: true,
+                createdAt: Date.now(),
+            }
+
+            await crateInitialData([initData]);
+
+            let mockWallet = jest.spyOn(Wallet, 'findOne');
+            mockWallet.mockResolvedValueOnce(initData);
+            let mockMutation = jest.spyOn(Mutation, 'findOne');
+            mockMutation.mockResolvedValueOnce(null);
+
+            mockWallet = jest.spyOn(Wallet, 'findOneAndUpdate');
+            mockWallet.mockResolvedValueOnce(sumData);
+            mockMutation = jest.spyOn(Mutation, 'create');
+            mockMutation.mockResolvedValueOnce(null);
+
+            // When
+            const result = await walletService.deposits(params, null);
+
+
+            // Then
+            expect(result.balance).toBe(100000);
+
+        });
+    })
+
+    describe('Withdrawals', () =>{
+
+        it('should success' , async ()=>{
+            // Given
+            const initData = {
+                customerId: 'abc123',
+                balance: 250000,
+                enabled: true,
+                createdAt: Date.now(),
+            };
+            const params = {
+                xid: 'abc123',
+                amount: 50000,
+                reference_id: 'referenceid123'
+            }
+            const updatedData = {
+                customerId: 'abc123',
+                balance: 200000,
+                enabled: true,
+                createdAt: Date.now(),
+            }
+
+            await crateInitialData([initData]);
+
+            let mockWallet = jest.spyOn(Wallet, 'findOne');
+            mockWallet.mockResolvedValueOnce(initData);
+            let mockMutation = jest.spyOn(Mutation, 'findOne');
+            mockMutation.mockResolvedValueOnce(null);
+
+            mockWallet = jest.spyOn(Wallet, 'findOneAndUpdate');
+            mockWallet.mockResolvedValueOnce(updatedData);
+            mockMutation = jest.spyOn(Mutation, 'create');
+            mockMutation.mockResolvedValueOnce(null);
+
+            // When
+            const result = await walletService.deposits(params, null);
+
+
+            // Then
+            expect(result.balance).toBe(200000);
+        });
+    });
+
+});
